f3835308509ff34a257fc5a9e12d8a465454fa31
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-03-03 19:35:57 +0530

    Create 0005.Minimum Time to Complete Trips.java

---
+++ binarysearch/on answers/0005.Minimum Time to Complete Trips.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long minimumTime(int[] time, int totalTrips) {
+        long left = 1;
+        long right = (long) Arrays.stream(time).min().getAsInt() * totalTrips;
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            if (possible(time, mid, totalTrips)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    private boolean possible(int[] time, long givenTime, int totalTrips) {
+        long actualTrips = 0;
+        for (int t : time) {
+            actualTrips += givenTime / t;
+            if (actualTrips >= totalTrips) {
+                return true;  
+            }
+        }
+        return actualTrips >= totalTrips;
+    }
+}
