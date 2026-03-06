0e4fa38d66dead06a888818cc6f275a201be417e
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-03-06 22:51:34 +0530

    Create 0002.Spiral Matrix II.java

---
+++ 2D-Matrix/0002.Spiral Matrix II.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int ans[][]=new int [n][n];
+        int left=0,right=n-1,top=0,bottom=n-1;
+        int index=1;
+
+        while(left<=right && top<=bottom){
+        for(int i=left;i<=right;i++){
+              ans[top][i]=index;
+              index++;
+        }
+        top++;
+       for(int i=top;i<=bottom;i++){
+        ans[i][right]=index;
+        index++;
+       } 
+       right--;
+       for(int i=right;i>=left;i--){
+        ans[bottom][i]=index;
+        index++;
+       }
+       bottom--;
+       for(int i=bottom;i>=top;i--){
+        ans[i][left]=index;
+        index++;
+       }
+       left++;
+    }
+    return ans;
+}
+}
