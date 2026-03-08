e2315664e7dc9ecd7714e56c09e0a8ff83377fb9
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-03-08 22:13:12 +0530

    Update 0029.linearsearch.java

--- arrays/0029.linearsearch.java
+++ arrays/0029.linearsearch.java
@@ -3,7 +3,9 @@ class Solution {
         // code here
         int ans=-1;
      for(int i=0;i<arr.length;i++){
-         if(arr[i]==x)ans=i;
+         if(arr[i]==x){ans=i;
+         break;
+     }
      }
      return ans;
     }
