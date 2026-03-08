2f091ec64875978461f47573fa11c68fc838f986
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-03-08 19:54:41 +0530

    Update 0026.largest element in array.java

--- arrays/0026.largest element in array.java
+++ arrays/0026.largest element in array.java
@@ -3,7 +3,7 @@ class Solution {
         // code here
         int maxi=0;
        for(int i=0;i<arr.length;i++){
-          maxi= Math.Max(maxi,arr[i]);
+          maxi= Math.max(maxi,arr[i]);
        }
        return maxi;
     }
