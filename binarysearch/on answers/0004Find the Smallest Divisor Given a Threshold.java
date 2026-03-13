a59277508d5c337f5ace7baa25c10034f974cd93
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-03-13 16:39:40 +0530

    Update 0004Find the Smallest Divisor Given a Threshold.java

--- binarysearch/on answers/0004Find the Smallest Divisor Given a Threshold.java
+++ binarysearch/on answers/0004Find the Smallest Divisor Given a Threshold.java
@@ -1,26 +1,29 @@
 class Solution {
-    public boolean divisor(int []nums,int threshold,int divisior){
-        int ans=0;
-        for(int array:nums){
-            ans += (array + divisior - 1) / divisior;
+      public boolean isAns(int[] nums, int k, int ans){
+        int sum = 0;
+        for(int num : nums){
+            sum += Math.ceil((double)num/ans); 
         }
-            return ans<=threshold;
-        }
+        return sum<=k;
+    }
     public int smallestDivisor(int[] nums, int threshold) {
-        int low=1;
-        int high=0;
-        for(int w:nums){
-             high=Math.max(w,high);
+        // Exact same as Leetcode - 875 KoKo Eating Bananas
+        int l = 1;
+        int r = Integer.MIN_VALUE;
+        for(int num : nums){
+            l = Math.min(l, num);
+            r = Math.max(r, num);
         }
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(divisor(nums,threshold,mid)){
-                 high=mid-1;
+        int ans = 0;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(isAns(nums, threshold, mid)){
+                ans = mid;
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
         }
-        else{
-            low=mid+1;
-        }
-        }
-        return low;
+        return ans;
     }
 }
