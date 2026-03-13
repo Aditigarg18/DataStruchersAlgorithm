eb41cff058451ed9f5a4c69c4a18db1ffb51c1b3
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-03-13 09:19:05 +0530

    Update 0003.peak element.java

--- binarysearch/on1DArrays/0003.peak element.java
+++ binarysearch/on1DArrays/0003.peak element.java
@@ -1,40 +1,12 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-      int n=nums.length;
-      if(n==1){
-        return 0;
-      }
-      int low=0,high=n-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(mid==0){
-                if(nums[mid]>nums[mid+1]){
-                    return mid;
-                }
-                else{
-                    low=mid+1;
-                }
-            }
-                else if(mid==n-1){
-                    if(nums[mid]>nums[mid-1]){
-                        return mid;
-                    }
-                    else{
-                        high=mid-1;
-                    }
-                }
-                else{
-                    if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]){
-                        return mid;
-                    }
-                    else if(nums[mid]<nums[mid+1]){
-                        low=mid+1;
-                    }
-                    else{
-                        high=mid-1;
-                    }
-                }
-            }
-            return-1;
-    }
+     int l = 0, h = nums.length-1;
+      while(l < h){ 
+        int m = l + (h-l)/2; 
+        if(nums[m] > nums[m+1])
+         h = m; 
+        else l = m + 1; 
+         } 
+         return l;
+          }
 }
