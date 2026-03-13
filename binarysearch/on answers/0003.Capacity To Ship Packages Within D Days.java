cef07213249321cc3e30054a7d3bd416d6a58fb7
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-03-13 15:29:20 +0530

    Update 0003.Capacity To Ship Packages Within D Days.java

--- binarysearch/on answers/0003.Capacity To Ship Packages Within D Days.java
+++ binarysearch/on answers/0003.Capacity To Ship Packages Within D Days.java
@@ -1,37 +1,37 @@
 class Solution {
-    public boolean isPossible(int mid,int[]weights,int days){
-        int count=1;
-        int sum=0;
-        for(int i=0;i<weights.length;i++){
-            sum+=weights[i];
-        if(sum>mid){
-           count++;
-           sum=weights[i];
-        }
-    }
-    return count<=days;
-    }
     public int shipWithinDays(int[] weights, int days) {
-        int maxcapacity=0;
-        for(int w:weights){
-            maxcapacity+=w;
+               int maxi = Integer.MIN_VALUE, sum = 0;
+        for (int i = 0; i < weights.length; i++) {
+            sum += weights[i];
+            maxi = Math.max(maxi, weights[i]);
         }
-        int low=0;
-        for(int w:weights){
-            low=Math.max(low,w);
+          int low=maxi;
+          int high=sum;
+          while(low<=high){
+            int mid=(low+high)/2;
+            if(possible(weights,mid)<=days){
+               high=mid-1;
+            }else{
+                low=mid+1;
+            }
+           
+          }
+           return low;
+    }
+    
+    public int possible(int[] weights,int mid){
+        int n=weights.length;
+        int day=1;
+        int load=0;
+        for(int i=0;i<n;i++){
+            if(load+weights[i]>mid){
+                day++;
+                load=weights[i];
+            }else{
+                load+=weights[i];
+            }
         }
-        int high=maxcapacity;
-        int result=0;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(isPossible(mid,weights,days)){
-            result= mid;
-            high=mid-1;
-        }
-        else{
-            low=mid+1;
-        }
-        }
-        return result;
+
+          return day;
     }
 }
