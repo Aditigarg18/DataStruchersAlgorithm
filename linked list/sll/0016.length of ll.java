bd6a3e0f4bf77da678cf8b5c327e3abbcfa7b460
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-04-10 15:04:47 +0530

    Create 0016.length of ll.java

---
+++ linked list/sll/0016.length of ll.java
@@ -0,0 +1,34 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public int lengthOfLoop(Node head) {
+        // code here
+      Node slow=head;
+     Node fast=head;
+       int count=0;
+       while(fast!=null && fast.next!=null){
+           slow=slow.next;
+           fast=fast.next.next;
+           if(slow==fast){
+               count=1;
+               slow=slow.next;
+               while(slow!=fast){
+                   slow=slow.next;
+                   count++;
+           }
+         return count;
+       }
+    }
+    return 0;
+    }
+}
