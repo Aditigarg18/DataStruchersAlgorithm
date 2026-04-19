e3670a00ccd3054df15be3c7897dbc650c742215
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-04-19 13:13:33 +0530

    Update 0042.right rotate array by one.java

--- arrays/0042.right rotate array by one.java
+++ arrays/0042.right rotate array by one.java
@@ -3,6 +3,7 @@ class Solution {
         // code here
    int n = arr.length;
          //for right rotation store last element and traverse from last and shift element 
+        //for left rotation store first element and traverse from start and shift element
         int last = arr[n - 1]; // store last element
         
         // shift elements right
