7f25eb92cdc74735417fad544ac2f47759f8c28a
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-04-19 12:24:34 +0530

    Update 0031.check if array is sorted and rotated.java

--- arrays/0031.check if array is sorted and rotated.java
+++ arrays/0031.check if array is sorted and rotated.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean check(int[] nums) {
+         //do for false,count the number of breaks.
+        //if the number of breaks are less than 1  or equal,then true
    int n=nums.length;
    int count=0;
    for(int i=1;i<n;i++){
