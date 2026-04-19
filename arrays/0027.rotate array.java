b5055682c41b1e97f83820f8dc54d6bef25671f0
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-04-19 13:14:36 +0530

    Update 0027.rotate array.java

--- arrays/0027.rotate array.java
+++ arrays/0027.rotate array.java
@@ -1,10 +1,14 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+//right rotation
         int n=nums.length;
         k=k%n;
         reverse(nums,0,n-1);
         reverse(nums,0,k-1);
         reverse(nums,k,n-1);
+        //left      reverse(arr, 0, d - 1);     // step 1
+       // reverse(arr, d, n - 1);     // step 2
+       // reverse(arr, 0, n - 1); 
     }
     public void reverse(int[]nums,int start,int end){
         while(start<=end){
