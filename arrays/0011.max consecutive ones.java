bae4afc1959a872c337867c442188f5b7194b07b
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-04-20 13:45:02 +0530

    Update 0011.max consecutive ones.java

--- arrays/0011.max consecutive ones.java
+++ arrays/0011.max consecutive ones.java
@@ -1,15 +1,14 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int n=nums.length;
         int count=0;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==1)
-            count++;
-            else
-            count=0;
-            ans=Math.max(ans,count);
+        int maxi=0;
+        for(int i=0;i<nums.length;i++){
+             if(nums[i]==1){
+                count++;
+              maxi= Math.max(maxi,count);
+             }
+             else if(nums[i]==0)count=0;
         }
-        return ans;
+        return maxi;
     }
 }
