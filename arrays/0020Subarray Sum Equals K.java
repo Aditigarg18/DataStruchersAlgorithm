8354c7980c52a0dbffe1be11cfb37ddac86d49c8
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-04-21 09:04:36 +0530

    Add subarraySum method to count subarrays with sum k
    Implement subarraySum method to count subarrays summing to k.

--- arrays/0020Subarray Sum Equals K.java
+++ arrays/0020Subarray Sum Equals K.java
@@ -15,3 +15,19 @@ for(int i=0;i<nums.length;i++){
 return count;
     }
 }
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+      int n=nums.length;
+      int count=0;
+      for(int i=0;i<nums.length;i++){
+       int sum=0;
+        for(int j=i;j<n;j++){
+            sum=sum+nums[i];
+            if(sum==k)
+            count++;
+        }
+      }  
+      return count;
+    }
+}
