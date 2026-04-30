7b1d908781adc6231d1d1aa05fad5f013aaae902
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-04-30 18:39:41 +0530

    Create 0010.Maximum Nesting Depth of the Parentheses.java

---
+++ Strings/0010.Maximum Nesting Depth of the Parentheses.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxDepth(String s) {
+        int count=0;
+        int maxi=0;
+       for(char c:s.toCharArray()){
+        if(c=='('){
+            count++;
+            maxi=Math.max(count,maxi);
+        }
+          if(c==')')count--;
+       } 
+       return maxi;
+    }
+}
