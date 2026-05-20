329a46a08bdfbafde7561ae1e2142dcbc414bdba
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-05-20 21:20:45 +0530

    Create 022.sort list.java

---
+++ linked list/sll/022.sort list.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+    //Brute force approach
+    // ArrayList<Integer>ans=new ArrayList<>();
+    // ListNode temp=head;
+    // while(temp!=null){
+    //     ans.add(temp.val);
+    //     temp=temp.next;
+    // }
+    //    Collections.sort(ans);
+    //    temp=head;
+    //    for(int i=0;i<ans.size();i++){
+    //     temp.val=ans.get(i);
+    //     temp=temp.next;
+    //    }  
+    //    return head;
+
+    //optimal approach merge sort
+    if(head==null ||head.next==null){
+        return head;
+    }
+        ListNode middle=findMiddle(head);
+        ListNode right=middle.next;
+        middle.next=null;
+        ListNode left=head;
+        left=sortList(left);
+        right=sortList(right);
+        return mergedtwo(left,right);
+    }
+    public ListNode mergedtwo(ListNode list1,ListNode list2){
+    ListNode dummyNode=new ListNode(-1,null);
+    ListNode temp=dummyNode;
+    while(list1!=null && list2!=null){
+        if(list1.val<=list2.val){
+            temp.next=list1;
+            list1=list1.next;
+        }
+        else{
+            temp.next=list2;
+            list2=list2.next;
+        }
+        temp=temp.next;
+    }
+    if(list1!=null){
+        temp.next=list1;
+    }
+    else{
+        temp.next=list2;
+    }
+    return dummyNode.next;
+    }
+    public ListNode findMiddle(ListNode head){
+        if(head==null ||head.next==null){
+            return head;
+        }
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
