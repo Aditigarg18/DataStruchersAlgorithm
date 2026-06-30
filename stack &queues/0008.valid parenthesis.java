ba649c41e71486492e032655fe30d1e7541fc48f
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-06-30 13:18:10 +0530

    Update 0008.valid parenthesis.java

--- stack &queues/0008.valid parenthesis.java
+++ stack &queues/0008.valid parenthesis.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character>st=new Stack<>();
-       for(char ch:s.toCharArray()){
-        if(ch=='('||ch=='{'||ch=='['){
-            st.push(ch);
-        }
-        else {
-            if(st.isEmpty())
-            return false;
-        char top=st.peek();
-        if((ch==')' && top=='(')||
-            (ch=='}' && top=='{')||
-            (ch==']' && top=='[')){
-            st.pop();
-       }
-       else{
-        return false;
-       }
-        }
-    }
-    return st.isEmpty();
+     Stack<Character>st=new Stack<>();
+     for(int i=0;i<s.length();i++){
+if(s.charAt(i)=='('||s.charAt(i)=='{' || s.charAt(i)=='[')
+     {
+        st.push(s.charAt(i));
+     }
+     else {
+     if (st.isEmpty())return false;
+     
+     char ch=st.peek();
+     if((ch=='(' && s.charAt(i)==')')||
+     (ch=='[' && s.charAt(i)==']')||
+     (ch=='{' && s.charAt(i)=='}'))
+     st.pop();
+     else return false;
+     }
+     }
+     return st.isEmpty();
     }
 }
+        
+       
