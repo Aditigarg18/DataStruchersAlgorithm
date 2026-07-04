45952431341df6d2e0bcbc019f80fe216114a6dd
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-07-04 10:11:58 +0530

    Update 0010.next smallest element.java

--- stack &queues/0010.next smallest element.java
+++ stack &queues/0010.next smallest element.java
@@ -1,18 +1,16 @@
 class Solution {
     static ArrayList<Integer> nextSmallerEle(int[] arr) {
         // code here
-        ArrayList<Integer>ans=new ArrayList<>();
+          int n=arr.length;
+        ArrayList<Integer>ans=new ArrayList<>(Collections.nCopies(n,-1));
         Stack<Integer>st=new Stack<>();
-        int n=arr.length;
         for(int i=n-1;i>=0;i--){
             while(!st.isEmpty() && st.peek()>=arr[i]){
                 st.pop();
             }
-            if(st.isEmpty()){
-               ans.add(0,-1);
-            }
-            else{
-                ans.add(0,st.peek());
+            
+            if(!st.isEmpty()){
+                ans.set(i,st.peek());
             }
                 st.push(arr[i]);
         }
