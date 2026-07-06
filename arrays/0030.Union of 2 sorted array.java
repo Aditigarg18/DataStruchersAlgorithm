39591c8cf7f42e8543fa75765f9899cd353aabe4
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-07-06 20:17:56 +0530

    Update 0030.Union of 2 sorted array.java

--- arrays/0030.Union of 2 sorted array.java
+++ arrays/0030.Union of 2 sorted array.java
@@ -1,5 +1,44 @@
 class Solution {
     public static ArrayList<Integer> findUnion(int a[], int b[]) {
+       int i=0,j=0;
+       int m=a.length;
+       int n=b.length;
+       ArrayList<Integer>ans=new ArrayList<>();
+       while(i<m && j<n){
+           if(a[i]<b[j]){
+               if(ans.isEmpty() || ans.get(ans.size()-1)!=a[i])
+                   ans.add(a[i]);
+                   i++;
+               }
+               else if(b[j]<a[i]){
+                     if(ans.isEmpty() || ans.get(ans.size()-1)!=b[j])
+                         ans.add(b[j]);
+                         j++;
+               }
+               else{
+      if(ans.isEmpty() || ans.get(ans.size()-1)!=a[i])
+                   ans.add(a[i]);
+                   i++;
+                   j++;
+               
+           }
+       }
+        while(i<m){
+              if(ans.isEmpty() || ans.get(ans.size()-1)!=a[i])
+                  ans.add(a[i]);
+                  i++;
+        }
+        while(j<n){
+              if(ans.isEmpty() || ans.get(ans.size()-1)!=b[j])
+                  ans.add(b[j]);
+                  j++;
+        }
+       return ans; 
+    }
+}
+
+class Solution {
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
         // code here
         HashSet<Integer>ans=new HashSet<>();
         for(int i=0;i<a.length;i++){
