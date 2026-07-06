36c99b21cd4e19bafaf5dd007c3b423792a690fa
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-07-06 17:02:46 +0530

    Update 0017.single number.java

--- arrays/0017.single number.java
+++ arrays/0017.single number.java
@@ -7,3 +7,19 @@ class Solution {
         return ans;
     }
 }
+//brute force
+       class Solution {
+    public int singleNumber(int[] nums) {
+        int num=0,count=0,ans=0;
+        for(int i=0;i<nums.length;i++){
+           num=nums[i];
+            count=0;
+        for(int j=0;j<nums.length;j++){
+            if(num==nums[j])
+            count++;
+        }
+         if (count==1) return num;
+    }
+    return -1;
+}
+       }
