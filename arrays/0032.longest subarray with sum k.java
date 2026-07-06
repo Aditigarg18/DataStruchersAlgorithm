fbb4e3b7e2a89fcfaaf65426d41d1c93822ca937
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-07-06 15:54:54 +0530

    Update 0032.longest subarray with sum k.java

--- arrays/0032.longest subarray with sum k.java
+++ arrays/0032.longest subarray with sum k.java
@@ -1,5 +1,23 @@
 // User function Template for Java
-
+//brute force
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        // code here
+        //brute force
+        int sum=0;
+        int  maxi=0;
+        for(int i=0;i<arr.length;i++){
+            sum=0;
+            for(int j=i;j<arr.length;j++){
+                sum=sum+arr[j];
+                if(sum==k)
+                maxi=Math.max(maxi,j-i+1);
+            }
+        }
+        return maxi;
+    }
+}
+//gives tle
 class Solution {
     public int longestSubarray(int[] arr, int k) {
         // code here
