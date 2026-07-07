086577a6f1d51181867c6ddba6da4cf974a1e298
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-07-07 21:35:38 +0530

    Update 0008.move zeroes.java

--- arrays/0008.move zeroes.java
+++ arrays/0008.move zeroes.java
@@ -14,3 +14,17 @@ class Solution {
 
     }
 }
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int index=0;
+        int temp[]=new int[n];
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0)
+            temp[index++]=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=temp[i];
+        }
+    }
+}
