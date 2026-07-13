e1e240698cc63c63a9fb6257218637b7abd2b8f4
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-07-13 12:29:18 +0530

    Update 0004.two sum.java

--- arrays/0004.two sum.java
+++ arrays/0004.two sum.java
@@ -40,3 +40,26 @@ class Solution {
         return new int[]{};
     }
 }
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       int n=nums.length;
+    int [][]arr=new int[n][2];
+    for(int i=0;i<n;i++){
+        arr[i][0]=nums[i];
+        arr[i][1]=i;
+    }
+    Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
+    int l=0;
+       int r=nums.length-1;
+       while(l<r){
+        int sum=arr[l][0]+arr[r][0];
+        if(sum==target) {
+            return new int[]{arr[l][1],arr[r][1]};
+        }
+        else if(sum>target)
+        r--;
+        else l++;
+       }
+       return new int[]{};
+    }
+}
