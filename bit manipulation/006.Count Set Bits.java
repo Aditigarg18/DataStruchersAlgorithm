cf9e3bb80b3cde44374ca1c7c3a9b7b449710629
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-07-14 12:15:00 +0530

    Create 006.Count Set Bits.java

---
+++ bit manipulation/006.Count Set Bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int setBits(int n) {
+        // code here
+        int count=0;
+        while(n>1){
+          count+=(n&1);
+          n=n>>1;
+        }
+        if(n==1)count+=1;
+        return count;
+    }
+}
